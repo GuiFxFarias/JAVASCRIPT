a004bd56a2c2576caa6514b06b4a244511903a98
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-08 10:15:47 -0300

    att

--- exercicios/teste de Js/script.js
+++ exercicios/teste de Js/script.js
@@ -1,25 +1,71 @@
-function verificar(){
-    var text = document.getElementById('nome')
+function verifica(){
+    var fnome = document.getElementById('nome').value
     var resultado = document.getElementById('res')
-    
-   
-    
-    if (text.length == 0 || text != String){
-        window.alert('Verifique os dados e tente novamente')
-    } else{
-        img.setAttribute('id', 'foto')  
-        var img = document.createElement('img')
-        var fParticipa = document.getElementsByName('participa')
-        var gols = ''
-        var assists = ''
 
-    } if (fParticipa[0].checked){
-        if (text == 'Xavi')
-        var gols = 85
-        img.setAttribute('src', 'imagens/xavi-redondo.png')
+    if (fnome.length == 0) {
+        window.alert('Verifique os dados e tente novamente ou digite o nome do jogador como exemplificado')
+    }else {
+        var gols = ''.value
+        var assists = ''.value
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        var fparticipa = document.getElementsByName('participa')
+
+        if (fparticipa[0].checked){
+            if (fnome == 'Xavi'){
+                gols = 85
+                img.setAttribute('src', 'imagens/xavi-redondo.png')
+            }
+            if (fnome == 'Messi'){
+                gols = 672	
+                img.setAttribute('src', 'imagens/messi-redondo.png')
+            }
+            if (fnome == 'Piqué'){
+                gols = 53
+                img.setAttribute('src', 'imagens/pique-redondo.png')
+            }
+            if (fnome == 'Iniesta'){
+                gols = 57
+                img.setAttribute('src', 'imagens/iniesta-redondo.png')
+            }
+            if (fnome == 'Puyol'){
+                gols = 19
+                img.setAttribute('src', 'imagens/puyol-redondo.png')
+            }
+            if (fnome == 'Busquets'){
+                gols = 18
+                img.setAttribute('src', 'imagens/busquets-redondo.png')
+            }
+        }else if (fparticipa[1].checked){
+            if (fnome == 'Xavi'){
+                assists = 185
+                img.setAttribute('src', 'imagens/xavi-redondo.png')
+            }
+            if (fnome == 'Messi'){
+                assists = 303
+                img.setAttribute('src', 'imagens/messi-redondo.png')
+            }
+            if (fnome == 'Piqué'){
+                assists = 15
+                img.setAttribute('src', 'imagens/pique-redondo.png')
+            }
+            if (fnome == 'Puyol'){
+                assists = 13
+                img.setAttribute('src', 'imagens/puyol-redondo.png')
+            }
+            if (fnome == 'Busquets'){
+                assists = 43
+                img.setAttribute('src', 'imagens/busquets-redondo.png')
+            }
+            if (fnome == 'Iniesta'){
+                assists = 138
+                img.setAttribute('src', 'imagens/iniesta-redondo.png')
+            }
+        }
+        resultado.style.textAlign = 'center'
+        resultado.innerHTML = `O ${fnome} tem em sua carreira ${gols || assists} ${fparticipa[0].value || fparticipa[1].value}`
+        resultado.appendChild(img)
     }
-    resultado.style.textAlign = 'center'
-    resultado.innerHTML = `O ${text}`
 }
 
 
