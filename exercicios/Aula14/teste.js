8c26bed242d02027e16fd17db3a53ad70e504a17
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-08 15:15:20 -0300

    teste de while com if

---
+++ exercicios/Aula14/teste.js
@@ -0,0 +1,15 @@
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c < 6)
+*/
+
+// FOR subsititui o WHILE
+
+console.log('Vai começar...')
+for(c = 1; c < 6; c++){
+    console.log(c)
+}
+console.log('FIM!')
---
+++ exercicios/teste de Js2/while-if.js
@@ -0,0 +1,17 @@
+a = 1
+b = 3
+
+while(a <= 10){
+    console.log(a)
+    if(a > b){
+        console.log(`Agora falta mais ${10-a}`)
+        console.log()
+    } else{ 
+        console.log('Vamos lá')
+        console.log()
+    }
+    a++
+}
+console.log()
+console.log('Voce terminou e ganhou de B')
+
