ec6d27a7187997e344c6eab52c60c9e587af8221
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-09 08:32:51 -0300

    exercicio-feito

--- exercicios/Exercicio-aula14/script.js
+++ exercicios/Exercicio-aula14/script.js
@@ -4,6 +4,38 @@ function contar(){
     let passo = document.getElementById('passo')
     let resultado = document.getElementById('res')
 
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        resultado.innerHTML = 'Impossivel contar'
+    } else {
+        resultado.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        let c = i
+
+        if (p <= 0){
+            window.alert('Passo = 0, mudando Passo para ')
+            p = 1
+        }
+
+        // contagem crescente
+        if (i < f){
+            while (c <= f){
+                c += p
+                resultado.innerHTML += `${c} \u{1F449}`
+            }
+        }else {
+            // contagem regressiva
+            while (c <= f){
+                c += p
+                resultado.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        resultado.innerHTML += `\u{1F3C1}`
+    }
+
+
+    /*
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
         resultado.innerHTML = 'Impossível contar'
         // window.alert('Verifique os dados novamente')
@@ -20,7 +52,7 @@ function contar(){
 
         if (i < f){
             // Contagem crescente
-            for(let c = i; c <= f; c+= p){
+            for(let c = i; c <= f; c += p){
                 resultado.innerHTML += `${c} \u{1F449}`
             }
         }else{
@@ -31,4 +63,6 @@ function contar(){
         } 
         resultado.innerHTML += `\u{1F3C1}`
     }
+    */
+
 }
