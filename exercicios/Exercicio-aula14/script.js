f1211fb422ef27d671025dba1bbd9600ed03a32c
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-09 08:00:36 -0300

    exercicio corrigido

--- exercicios/Exercicio-aula14/script.js
+++ exercicios/Exercicio-aula14/script.js
@@ -1,8 +1,34 @@
-a = 0
-b = 10
-p = 2
+function contar(){
+    let ini = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let resultado = document.getElementById('res')
 
-while(p < b){
-    p = p + p + a
-    console.log(p-4)
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
+        resultado.innerHTML = 'Impossível contar'
+        // window.alert('Verifique os dados novamente')
+    } else{
+        resultado.innerHTML = 'contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0){
+            window.alert('Passo invalido, considerando passo 1')
+            p = 1
+        }
+
+        if (i < f){
+            // Contagem crescente
+            for(let c = i; c <= f; c+= p){
+                resultado.innerHTML += `${c} \u{1F449}`
+            }
+        }else{
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                resultado.innerHTML += `${c} \u{1F449}`
+            }
+        } 
+        resultado.innerHTML += `\u{1F3C1}`
+    }
 }
