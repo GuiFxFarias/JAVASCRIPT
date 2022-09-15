2d3e1076e3fb18cac114f391b4400c61fabae3bb
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-15 09:01:10 -0300

    testes

--- exercicios/Aula15/ambiente.js
+++ exercicios/Aula15/ambiente.js
@@ -1,10 +1,12 @@
 let num = [5,8,2,9,3]
 
-
+/*
 num[5] = 6
 num.push(0)
 
 
-
+*/
 let pos = num.indexOf(2)
 console.log(pos)
+
+
--- exercicios/Aula15/vertor-natela.js
+++ exercicios/Aula15/vertor-natela.js
@@ -1,13 +1,26 @@
-let valores = [0,9,2,1,7,8]
-valores.sort()
+let valores = [0,1,2,3,9,5,6]
+
 
 /*
 for (let pos = 0; pos < valores.length; pos++){
     console.log(valores[pos])
 }
-*/
+
 
 for(let pos in valores){
     console.log(valores[pos])
 }
+*/
 
+let info = valores.indexOf(1)
+
+for (let pos in valores){
+    let print = valores[pos]
+    if (print != info ){
+        console.log(`O valor ${valores[pos]} é incompativel`)
+    }else if (print == info){
+        console.log(`Este valor é o ${info}`)
+    }else{
+        console.log(valores[pos])
+    }
+}
