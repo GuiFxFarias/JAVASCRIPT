44c10fe8cf7bdad3b5e05851bafc9f05990022d9
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-09-15 08:39:33 -0300

    js.add

---
+++ exercicios/Aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5,8,2,9,3]
+
+
+num[5] = 6
+num.push(0)
+
+
+
+let pos = num.indexOf(2)
+console.log(pos)
---
+++ exercicios/Aula15/vertor-natela.js
@@ -0,0 +1,13 @@
+let valores = [0,9,2,1,7,8]
+valores.sort()
+
+/*
+for (let pos = 0; pos < valores.length; pos++){
+    console.log(valores[pos])
+}
+*/
+
+for(let pos in valores){
+    console.log(valores[pos])
+}
+
